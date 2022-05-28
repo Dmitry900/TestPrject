7d4e6bc60fe88d9d11a2c345af402096ed6eaacf
Dmitry900 <56482488+Dmitry900@users.noreply.github.com>
2022-05-28 18:27:50 +0700

    add project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int x = 0;  // общий ресурс
+ 
+
+List<Thread> threads = new();
+//запускаем потоки и блокируем главный поток
+for (int i = 1; i < 6; i++)
+{
+    Thread myThread = new(Print);
+    myThread.Name = $"Поток {i}";
+    myThread.Start();
+    myThread.Join();
+
+
+}
+Console.WriteLine("Готово");
+Console.ReadKey();
+
+void Print()
+{
+
+    x = 1;
+    for (int i = 1; i < 6; i++)
+    {
+        Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+        x++;
+        Thread.Sleep(1000);
+    }
+    
+}
+   
